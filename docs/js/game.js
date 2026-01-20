dc5dc94c129f0cd3cdf0bfe382c1447bb3c5b7d6
Kyle Winfield Burnham <kylewinfieldburnham@gmail.com>
2026-01-19 20:29:44 -0800

    added extract subtracting

--- docs/js/game.js
+++ docs/js/game.js
@@ -1,5 +1,17 @@
-let rocks = document.querySelector('.rocks')
+let rockCount = document.querySelector('.rock-count')
+let parsedRockCount = parseFloat(rockCount.innerHTML)
+
+let extractCost = document.querySelector('.extract-cost')
+let parsedExtractCost = parseFloat(extractCost.innerHTML)
 
 function incrementRocks() {
-  rocks.innerHTML = parseFloat(rocks.innerHTML) + 1
+  parsedRockCount += 1
+  rockCount.innerHTML = parsedRockCount
+}
+
+function buyExtract() {
+  if (parsedRockCount >= parsedExtractCost) {
+    parsedRockCount -= parsedExtractCost
+    rockCount.innerHTML = parsedRockCount
+  }
 }
