1386a3684c6811748c2fbebb497fb6e581ac12a5
Kyle Winfield Burnham <kylewinfieldburnham@gmail.com>
2026-01-20 00:09:58 -0800

    elements appear as you get them

--- docs/js/game.js
+++ docs/js/game.js
@@ -2,12 +2,15 @@ let rockResourceDisplay = document.querySelector('.rock-resource-display')
 let rockCountSource = document.querySelector('.rock-count')
 let rockCount = parseFloat(rockCountSource.innerHTML)
 
+let limestoneResourceDisplay = document.querySelector('.limestone-resource-display')
 let limestoneCountSource = document.querySelector('.limestone-count')
 let limestoneCount = parseFloat(limestoneCountSource.innerHTML)
 
+let coalResourceDisplay = document.querySelector('.coal-resource-display')
 let coalCountSource = document.querySelector('.coal-count')
 let coalCount = parseFloat(coalCountSource.innerHTML)
 
+let ironoreResourceDisplay = document.querySelector('.ironore-resource-display')
 let ironoreCountSource = document.querySelector('.ironore-count')
 let ironoreCount = parseFloat(ironoreCountSource.innerHTML)
 
@@ -21,6 +24,7 @@ function updateRockCount() {
 function incrementRocks() {
   rockCount += 15
   updateRockCount()
+  rockResourceDisplay.classList.remove("hidden")
 }
 
 function buyExtract() {
@@ -30,15 +34,18 @@ function buyExtract() {
 
     const roll = Math.floor(Math.random() * 10) + 1
     console.log(roll)
-    if (roll < 6) {
+    if (roll <= 6) {
       limestoneCount += 1
       limestoneCountSource.innerHTML = limestoneCount
-    } else if (roll < 9) {
+      limestoneResourceDisplay.classList.remove("hidden")
+    } else if (roll <= 9) {
       coalCount += 1
       coalCountSource.innerHTML = coalCount
+      coalResourceDisplay.classList.remove("hidden")
     } else {
       ironoreCount += 1
       ironoreCountSource.innerHTML = ironoreCount
+      ironoreResourceDisplay.classList.remove("hidden")
     }
   }
 }
