8bad771436b7b35bb9ed7eb9c1db1032584409a6
Kyle Winfield Burnham <kylewinfieldburnham@gmail.com>
2026-01-19 23:39:13 -0800

    earn limestone, coal and iron

--- docs/js/game.js
+++ docs/js/game.js
@@ -1,21 +1,42 @@
-let rockCount = document.querySelector('.rock-count')
-let parsedRockCount = parseFloat(rockCount.innerHTML)
+let rockCountSource = document.querySelector('.rock-count')
+let rockCount = parseFloat(rockCountSource.innerHTML)
 
-let extractCost = document.querySelector('.extract-cost')
-let parsedExtractCost = parseFloat(extractCost.innerHTML)
+let limestoneCountSource = document.querySelector('.limestone-count')
+let limestoneCount = parseFloat(limestoneCountSource.innerHTML)
+
+let coalCountSource = document.querySelector('.coal-count')
+let coalCount = parseFloat(coalCountSource.innerHTML)
+
+let ironCountSource = document.querySelector('.iron-count')
+let ironCount = parseFloat(ironCountSource.innerHTML)
+
+let extractCostSource = document.querySelector('.extract-cost')
+let extractCost = parseFloat(extractCostSource.innerHTML)
 
 function updateRockCount() {
-  rockCount.innerHTML = parsedRockCount
+  rockCountSource.innerHTML = rockCount
 }
 
 function incrementRocks() {
-  parsedRockCount += 1
+  rockCount += 1
   updateRockCount()
 }
 
 function buyExtract() {
-  if (parsedRockCount >= parsedExtractCost) {
-    parsedRockCount -= parsedExtractCost
+  if (rockCount >= extractCost) {
+    rockCount -= extractCost
     updateRockCount()
+
+    const roll = Math.floor(Math.random() * 10) + 1
+    if (roll < 6) {
+      limestoneCount += 1
+      limestoneCountSource.innerHTML = limestoneCount
+    } else if (roll < 9) {
+      coalCount += 1
+      coalCountSource.innerHTML = coalCount
+    } else {
+      ironCount += 1
+      ironCountSource.innerHTML = ironCount
+    }
   }
 }
