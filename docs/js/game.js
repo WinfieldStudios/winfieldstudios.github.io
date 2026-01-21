7790bf46c648af8d6bcc317cfc82869c5addc5d6
Kyle Winfield Burnham <kylewinfieldburnham@gmail.com>
2026-01-21 05:06:31 -0800

    added more purchasables
    they are not functional yet, but the user won't see them

--- docs/js/game.js
+++ docs/js/game.js
@@ -1,6 +1,8 @@
 // 
 // DATA
 
+let rockLevel = 1
+let pickaxeLevel = 1
 let rocksPerClick = 1
 let globalPurchaseMultiplier = 1
 
@@ -11,55 +13,6 @@ for (const res of Object.values(resources)) {
   console.log(res.name)
 }
 */
-const resources = {
-  rocks: {
-    name: 'rocks',
-    countSource: document.querySelector('.rock-count'),
-    displayContainer: document.querySelector('.rock-resource-display'),
-    get count() {
-      return parseFloat(document.querySelector('.rock-count').innerHTML)
-    },
-    set count(value) {
-      this.countSource.innerHTML = value
-    }
-  },
-  limestone: {
-    name: 'limestone',
-    countSource: document.querySelector('.limestone-count'),
-    displayContainer: document.querySelector('.limestone-resource-display'),
-    get count() {
-      return parseFloat(document.querySelector('.limestone-count').innerHTML)
-    },
-    set count(value) {
-      this.countSource.innerHTML = value
-    }
-  },
-  coal: {
-    name: 'coal',
-    countSource: document.querySelector('.coal-count'),
-    displayContainer: document.querySelector('.coal-resource-display'),
-    get count() {
-      return parseFloat(document.querySelector('.coal-count').innerHTML)
-    },
-    set count(value) {
-      this.countSource.innerHTML = value
-    }
-  },
-  ironore: {
-    name: 'ironore',
-    countSource: document.querySelector('.ironore-count'),
-    displayContainer: document.querySelector('.ironore-resource-display'),
-    get count() {
-      return parseFloat(document.querySelector('.ironore-count').innerHTML)
-    },
-    set count(value) {
-      this.countSource.innerHTML = value
-    }
-  }
-}
-
-const { rocks, limestone, coal, ironore } = resources
-
 const purchasables = {
   extract: {
     name: 'extract',
@@ -72,31 +25,196 @@ const purchasables = {
     set cost(value) {
       this.costSource.innerHTML = value
     }
+  },
+  upgrade: {
+    name: 'upgrade',
+    level: 0,
+    costSource: document.querySelector('.upgrade-cost'),
+    button: document.querySelector('.upgrade-button'),
+    get cost() {
+      return parseFloat(document.querySelector('.upgrade-cost').innerHTML)
+    },
+    set cost(value) {
+      this.costSource.innerHTML = value
+    }
+  },
+  roast: {
+    name: 'roast',
+    level: 0,
+    costSource: document.querySelector('.roast-cost'),
+    button: document.querySelector('.roast-button'),
+    get cost() {
+      return parseFloat(document.querySelector('.roast-cost').innerHTML)
+    },
+    set cost(value) {
+      this.costSource.innerHTML = value
+    }
+  },
+  blast: {
+    name: 'blast',
+    level: 0,
+    costSource: document.querySelector('.blast-cost'),
+    button: document.querySelector('.blast-button'),
+    get cost() {
+      return parseFloat(document.querySelector('.blast-cost').innerHTML)
+    },
+    set cost(value) {
+      this.costSource.innerHTML = value
+    }
+  },
+  smelt: {
+    name: 'smelt',
+    level: 0,
+    costSource: document.querySelector('.smelt-cost'),
+    button: document.querySelector('.smelt-button'),
+    get cost() {
+      return parseFloat(document.querySelector('.smelt-cost').innerHTML)
+    },
+    set cost(value) {
+      this.costSource.innerHTML = value
+    }
+  },
+  hire: {
+    name: 'hire',
+    level: 0,
+    costSource: document.querySelector('.hire-cost'),
+    button: document.querySelector('.hire-button'),
+    get cost() {
+      return parseFloat(document.querySelector('.hire-cost').innerHTML)
+    },
+    set cost(value) {
+      this.costSource.innerHTML = value
+    }
   }
 }
 
-const { extract } = purchasables
+const { extract, upgrade, roast, blast, smelt, hire } = purchasables
+
+const resources = {
+  rocks: {
+    name: 'rocks',
+    gross: 0,
+    countSource: document.querySelector('.rock-count'),
+    displayContainer: document.querySelector('.rock-resource-display'),
+    get count() {
+      return parseFloat(document.querySelector('.rock-count').innerHTML)
+    },
+    set count(value) {
+      if (value > this.count) {
+        this.gross += value - this.count
+      }
+      this.countSource.innerHTML = value
+      if (rocks.count >= extract.cost * globalPurchaseMultiplier) {
+        extract.button.classList.remove("hidden")
+        extract.button.classList.add("enabled")
+      } else {
+        extract.button.classList.remove("enabled")
+      }
+    }
+  },
+  limestone: {
+    name: 'limestone',
+    gross: 0,
+    countSource: document.querySelector('.limestone-count'),
+    displayContainer: document.querySelector('.limestone-resource-display'),
+    get count() {
+      return parseFloat(document.querySelector('.limestone-count').innerHTML)
+    },
+    set count(value) {
+      if (value > this.count) {
+        this.gross += value - this.count
+      }
+      this.countSource.innerHTML = value
+    }
+  },
+  coal: {
+    name: 'coal',
+    gross: 0,
+    countSource: document.querySelector('.coal-count'),
+    displayContainer: document.querySelector('.coal-resource-display'),
+    get count() {
+      return parseFloat(document.querySelector('.coal-count').innerHTML)
+    },
+    set count(value) {
+      if (value > this.count) {
+        this.gross += value - this.count
+      }
+      this.countSource.innerHTML = value
+    }
+  },
+  coke: {
+    name: 'coke',
+    gross: 0,
+    countSource: document.querySelector('.coke-count'),
+    displayContainer: document.querySelector('.coke-resource-display'),
+    get count() {
+      return parseFloat(document.querySelector('.coke-count').innerHTML)
+    },
+    set count(value) {
+      if (value > this.count) {
+        this.gross += value - this.count
+      }
+      this.countSource.innerHTML = value
+    }
+  },
+  ironore: {
+    name: 'ironore',
+    gross: 0,
+    countSource: document.querySelector('.ironore-count'),
+    displayContainer: document.querySelector('.ironore-resource-display'),
+    get count() {
+      return parseFloat(document.querySelector('.ironore-count').innerHTML)
+    },
+    set count(value) {
+      if (value > this.count) {
+        this.gross += value - this.count
+      }
+      this.countSource.innerHTML = value
+    }
+  },
+  pigiron: {
+    name: 'pigiron',
+    gross: 0,
+    countSource: document.querySelector('.pigiron-count'),
+    displayContainer: document.querySelector('.pigiron-resource-display'),
+    get count() {
+      return parseFloat(document.querySelector('.pigiron-count').innerHTML)
+    },
+    set count(value) {
+      if (value > this.count) {
+        this.gross += value - this.count
+      }
+      this.countSource.innerHTML = value
+    }
+  },
+  steel: {
+    name: 'steel',
+    gross: 0,
+    countSource: document.querySelector('.steel-count'),
+    displayContainer: document.querySelector('.steel-resource-display'),
+    get count() {
+      return parseFloat(document.querySelector('.steel-count').innerHTML)
+    },
+    set count(value) {
+      if (value > this.count) {
+        this.gross += value - this.count
+      }
+      this.countSource.innerHTML = value
+    }
+  }
+}
+
+const { rocks, limestone, coal, coke, ironore, pigiron, steel } = resources
 
 //
 // CLICKING THE ROCK
 
-function updateRockCount() {
-  rocks.countSource.innerHTML = rocks.count
-  if (rocks.count >= extract.cost * globalPurchaseMultiplier) {
-    extract.button.classList.remove("hidden")
-    extract.button.classList.add("purchasable")
-  } else {
-    extract.button.classList.remove("purchasable")
-  }
-}
-
-function incrementRocks(event) {
+function clickOnRock(event) {
   rocks.count += rocksPerClick
-  updateRockCount()
   rocks.displayContainer.classList.remove("hidden")
 
   const x = event.offsetX + (Math.floor(Math.random() * 20) + 1) * (Math.floor(Math.random() * 2) == 0 ? 1 : -1)
-  const y = event.offsetY - (80 + Math.floor(Math.random() * 30) + 1)
+  const y = event.offsetY - (50 + Math.floor(Math.random() * 30) + 1)
 
   const div = document.createElement('div')
   div.innerHTML = `+${Math.round(rocksPerClick)}`
@@ -117,10 +235,9 @@ const timeout = (div) => {
 //
 // PURCHASABLES
 
-function buyExtract() {
+function purchaseExtract() {
   if (rocks.count >= extract.cost * globalPurchaseMultiplier) {
     rocks.count -= extract.cost * globalPurchaseMultiplier
-    updateRockCount()
 
     const roll = Math.floor(Math.random() * 10) + 1
     
@@ -185,10 +302,10 @@ function load() {
     extract.button.classList.add("hidden")
   }
   if (rocks.count >= extract.cost) {
-    extract.button.classList.add("purchasable")
+    extract.button.classList.add("enabled")
     extract.button.classList.remove("hidden")
   } else {
-    extract.button.classList.remove("purchasable")
+    extract.button.classList.remove("enabled")
   }
 }
 
