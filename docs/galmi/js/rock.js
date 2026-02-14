b5b8a6e3c307f185435f9b5aab7f7fdbadbb8867
tonymercado5503-collab <tonymercado5503@gmail.com>
2026-02-13 23:00:27 -0800

    galmitalks?

--- docs/galmi/js/rock.js
+++ docs/galmi/js/rock.js
@@ -15,3 +15,20 @@ function clickOnRock(event) {
   div.classList.add('fade-up');
   timeout(div);
 }
+
+function galmiTalks() {
+  const galmiTalks = document.querySelector('.galmi-talks');
+  galmiTalks.style.display = 'block';
+  setTimeout(() => {
+    galmiTalks.style.display = 'none';
+  }, 3000);
+
+  const div = document.createElement('div');
+  div.innerHTML = `${sign}${formatNumber(amount)}`;
+  div.style.cssText = `color: var(--primary-color); position: absolute; top: ${y}px; left: ${x}px; font-size: ${fontSize}px; font-weight: lighter; font-family: "Pixelated"; pointer-events: none;`;
+  if (isPassive) div.style.color = 'var(--primary-color-faded)';
+  resource.displayContainer.appendChild(div);
+
+  div.classList.add('gain-resource-animation');
+  timeout(div);
+}
