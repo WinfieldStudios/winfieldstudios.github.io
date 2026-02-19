1ae34a16838bbbf10115c8406a78da02f0707de5
Kyle Winfield Burnham <kylewinfieldburnham@gmail.com>
2026-02-18 19:51:53 -0800

    nerf housing production

--- docs/galmi/js/main.js
+++ docs/galmi/js/main.js
@@ -93,12 +93,12 @@ function generateIncome(seconds) {
     concreteToAdd = 0;
     chromiumToAdd = 0;
 
-    pigironToAdd += Math.max(0, Math.floor((resourcesPerSecond * 10 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.5) - (resourcesPerSecond * 0.5 / 2))));
-    steelToAdd += Math.max(0, Math.floor((resourcesPerSecond * 1 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.5) - (resourcesPerSecond * 0.5 / 2))));
-    ferrochromeToAdd += Math.max(0, Math.floor((resourcesPerSecond * 0.2 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.1) - (resourcesPerSecond * 0.1 / 2))));
-    stainlesssteelToAdd += Math.max(0, Math.floor((resourcesPerSecond * 0.05 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.025) - (resourcesPerSecond * 0.025 / 2))));
-    concreteToAdd += Math.max(0, Math.floor((resourcesPerSecond * 0.01 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.005) - (resourcesPerSecond * 0.005 / 2))));
-    chromiumToAdd += Math.max(0, Math.floor((resourcesPerSecond * 0.001 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.0005) - (resourcesPerSecond * 0.0005 / 2))));
+    pigironToAdd += Math.max(0, Math.floor((resourcesPerSecond * 1 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.5) - (resourcesPerSecond * 0.5 / 2))));
+    steelToAdd += Math.max(0, Math.floor((resourcesPerSecond * 0.1 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.05) - (resourcesPerSecond * 0.05 / 2))));
+    ferrochromeToAdd += Math.max(0, Math.floor((resourcesPerSecond * 0.01 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.005) - (resourcesPerSecond * 0.005 / 2))));
+    stainlesssteelToAdd += Math.max(0, Math.floor((resourcesPerSecond * 0.001 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.0005) - (resourcesPerSecond * 0.0005 / 2))));
+    concreteToAdd += Math.max(0, Math.floor((resourcesPerSecond * 0.000001 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.0000005) - (resourcesPerSecond * 0.0000005 / 2))));
+    chromiumToAdd += Math.max(0, Math.floor((resourcesPerSecond * 0.000000001 + 0.5) + Math.floor(Math.random() * (resourcesPerSecond * 0.0000000005) - (resourcesPerSecond * 0.0000000005 / 2))));
     
     pigiron.count += pigironToAdd;
     steel.count += steelToAdd;
