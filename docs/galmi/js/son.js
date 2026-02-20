c2d10c12f3695d4f3cfb41d0f32d16d932fc5770
Kyle Winfield Burnham <kylewinfieldburnham@gmail.com>
2026-02-19 16:18:48 -0800

    update powerup stats regularly

--- docs/galmi/js/son.js
+++ docs/galmi/js/son.js
@@ -17,6 +17,14 @@ window.addEventListener("mousemove", (e) => {
   mouseY = e.clientY;
 });
 
+// UPDATE
+setInterval(() => {
+  document.getElementById('stats-total-powerups-golden').innerHTML = totalPowerupsGolden;
+  document.getElementById('stats-powerups-golden-multiplier').innerHTML = getPowerupRocksPerClickMultiplier();
+  document.getElementById('stats-powerups-golden-duration').innerHTML = Math.floor(getPowerupGoldenDuration() / 1000);
+  document.getElementById('stats-powerups-golden-spawnrate').innerHTML = `${Math.floor(min / 1000)}-${Math.floor(max / 1000)}s`;
+}, 500);
+
 // POWER UP OBJECT
 // we call it "SON" because it's Galmi's son that is falling
 const SON = {
