58292f554453aa1a1a97b29ecd9366a22f19a7e9
Kyle Winfield Burnham <kylewinfieldburnham@gmail.com>
2026-02-21 17:10:16 -0800

    clicked tracking bug fix

--- docs/galmi/js/save.js
+++ docs/galmi/js/save.js
@@ -21,6 +21,8 @@ function save(textPlayed = "SAVED!") {
   for (const purchasable of Object.values(purchasables)) {
     const key = `${purchasable.name}Level`;
     localStorage.setItem(key, JSON.stringify(purchasable.level));
+    const key2 = `${purchasable.name}Clicked`;
+    localStorage.setItem(key2, JSON.stringify(purchasable.clicked));
   }
 
   for (const resource of Object.values(resources)) {
@@ -81,6 +83,9 @@ function load() {
   for (const purchasable of Object.values(purchasables)) {
     const savedLevel = JSON.parse(localStorage.getItem(`${purchasable.name}Level`));
     if (savedLevel !== null) purchasable.level = savedLevel;
+
+    const savedClicked = JSON.parse(localStorage.getItem(`${purchasable.name}Clicked`));
+    if (savedClicked !== null) purchasable.clicked = savedClicked;
   }
 
   for (const resource of Object.values(resources)) {
@@ -93,30 +98,7 @@ function load() {
     }
   }
 
-  updateUpgradeGalmi();
-  updateUpgradePickaxe();
-  checkPurchasables();
-  document.querySelector('.purchased-total-extract').innerHTML = extract.level - 1;
-  document.querySelector('.purchased-total-blast').innerHTML = blast.level - 1;
-  document.querySelector('.purchased-total-smelt').innerHTML = smelt.level - 1;
-  document.querySelector('.purchased-total-reduce').innerHTML = reduce.level - 1;
-  document.querySelector('.purchased-total-refine').innerHTML = refine.level - 1;
-  document.querySelector('.purchased-total-mix').innerHTML = mix.level - 1;
-  document.querySelector('.purchased-total-produce').innerHTML = produce.level - 1;
-  document.querySelector('.purchased-total-workers').innerHTML = hire.level - 1;
-  document.querySelector('.purchased-total-housing').innerHTML = housing.level - 1;
-  document.querySelector('.purchased-total-promoted').innerHTML = upgradeWorker.level - 1;
-  document.getElementById('stats-total-seconds-played').innerHTML = timestamp(timeStats[1]);
-  document.getElementById('stats-timestamp-grow1').innerHTML = timestamp(timeStats[3])
-  document.getElementById('stats-timestamp-grow2').innerHTML = timestamp(timeStats[4])
-  document.getElementById('stats-timestamp-grow3').innerHTML = timestamp(timeStats[5])
-  document.getElementById('stats-timestamp-grow4').innerHTML = timestamp(timeStats[6])
-  document.getElementById('stats-timestamp-toolt4').innerHTML = timestamp(timeStats[8])
-  document.querySelector('.total-clicks-ever').innerHTML = totalClicksEver;
-  document.getElementById('stats-total-powerups-golden').innerHTML = totalPowerupsGolden;
-  document.getElementById('stats-powerups-golden-multiplier').innerHTML = getPowerupRocksPerClickMultiplier();
-  document.getElementById('stats-powerups-golden-duration').innerHTML = Math.floor(getPowerupGoldenDuration() / 1000);
-  document.getElementById('stats-cps-max').innerHTML = getClicksPerSecondRecord();
+  updateUI();
 
   const timeWhenPlayerSaved = JSON.parse(localStorage.getItem('timeWhenPlayerSaved'));
   if (timeWhenPlayerSaved !== null) {
@@ -148,6 +130,7 @@ function restart() {
   for (const purchasable of Object.values(purchasables)) {
     if (localStorage.getItem(`${purchasable.name}Level`) !== null) {
       purchasable.level = PURCHASABLES_STARTING_LEVEL;
+      purchasable.clicked = PURCHASABLES_STARTING_CLICKED;
     }
   }
 
@@ -161,21 +144,36 @@ function restart() {
     }
   }
 
+  updateUI();
+  scheduleNextSon();
+}
+
+function updateUI() {
+
   updateUpgradeGalmi();
   updateUpgradePickaxe();
   checkPurchasables();
   document.querySelector('.purchased-total-extract').innerHTML = extract.level - 1;
+  document.getElementById('purchased-clicked-extract').innerHTML = formatNumber(extract.clicked);
   document.querySelector('.purchased-total-blast').innerHTML = blast.level - 1;
+  document.getElementById('purchased-clicked-blast').innerHTML = formatNumber(blast.clicked);
   document.querySelector('.purchased-total-smelt').innerHTML = smelt.level - 1;
+    document.getElementById('purchased-clicked-smelt').innerHTML = formatNumber(smelt.clicked);
   document.querySelector('.purchased-total-reduce').innerHTML = reduce.level - 1;
+    document.getElementById('purchased-clicked-reduce').innerHTML = formatNumber(reduce.clicked);
   document.querySelector('.purchased-total-refine').innerHTML = refine.level - 1;
+    document.getElementById('purchased-clicked-refine').innerHTML = formatNumber(refine.clicked);
   document.querySelector('.purchased-total-mix').innerHTML = mix.level - 1;
+    document.getElementById('purchased-clicked-mix').innerHTML = formatNumber(mix.clicked);
   document.querySelector('.purchased-total-produce').innerHTML = produce.level - 1;
+    document.getElementById('purchased-clicked-produce').innerHTML = formatNumber(produce.clicked);
   document.querySelector('.purchased-total-workers').innerHTML = hire.level - 1;
+    document.getElementById('purchased-clicked-workers').innerHTML = formatNumber(hire.clicked);
   document.querySelector('.purchased-total-housing').innerHTML = housing.level - 1;
+    document.getElementById('purchased-clicked-housing').innerHTML = formatNumber(housing.clicked);
   document.querySelector('.purchased-total-promoted').innerHTML = upgradeWorker.level - 1;
+    document.getElementById('purchased-clicked-promoted').innerHTML = formatNumber(upgradeWorker.clicked);
   document.getElementById('stats-total-seconds-played').innerHTML = timestamp(timeStats[1]);
-  document.getElementById('stats-total-seconds-offline').innerHTML = timestamp(timeStats[2]);
   document.getElementById('stats-timestamp-grow1').innerHTML = timestamp(timeStats[3])
   document.getElementById('stats-timestamp-grow2').innerHTML = timestamp(timeStats[4])
   document.getElementById('stats-timestamp-grow3').innerHTML = timestamp(timeStats[5])
@@ -186,38 +184,4 @@ function restart() {
   document.getElementById('stats-powerups-golden-multiplier').innerHTML = getPowerupRocksPerClickMultiplier();
   document.getElementById('stats-powerups-golden-duration').innerHTML = Math.floor(getPowerupGoldenDuration() / 1000);
   document.getElementById('stats-cps-max').innerHTML = getClicksPerSecondRecord();
-  scheduleNextSon();
-}
-
-function clearSave() {
-  localStorage.clear();
-
-  localStorage.setItem('totalSecondsPlayed', JSON.stringify(TOTAL_SECONDS_PLAYED_STARTING_AMOUNT));
-  localStorage.setItem('totalClicksEver', JSON.stringify(TOTAL_CLICKS_EVER_STARTING_AMOUNT));
-  localStorage.setItem('rocksPerClick', JSON.stringify(ROCKS_PER_CLICK_STARTING_AMOUNT));
-  localStorage.getItem('hasExtractedAfterUpgradingPickaxe', JSON.stringify(false));
-
-  for (const purchasable of Object.values(purchasables)) {
-    localStorage.setItem(`${purchasable.name}Level`, JSON.stringify(PURCHASABLES_STARTING_LEVEL));
-  }
-  for (const resource of Object.values(resources)) {
-    localStorage.setItem(`${resource.name}Count`, JSON.stringify(RESOURCES_STARTING_COUNT));
-    localStorage.setItem(`${resource.name}Gross`, JSON.stringify(RESOURCES_STARTING_GROSS));
-  }
-  updateUpgradeGalmi();
-  updateUpgradePickaxe();
-  checkPurchasables();
-  document.querySelector('.purchased-total-extract').innerHTML = extract.level - 1;
-  document.querySelector('.purchased-total-blast').innerHTML = blast.level - 1;
-  document.querySelector('.purchased-total-smelt').innerHTML = smelt.level - 1;
-  document.querySelector('.purchased-total-reduce').innerHTML = reduce.level - 1;
-  document.querySelector('.purchased-total-refine').innerHTML = refine.level - 1;
-  document.querySelector('.purchased-total-mix').innerHTML = mix.level - 1;
-  document.querySelector('.purchased-total-produce').innerHTML = produce.level - 1;
-  document.querySelector('.purchased-total-workers').innerHTML = hire.level - 1;
-  document.querySelector('.purchased-total-housing').innerHTML = housing.level - 1;
-  document.querySelector('.purchased-total-promoted').innerHTML = upgradeWorker.level - 1;
-  document.getElementById('stats-total-seconds-played').innerHTML = totalSecondsPlayed;
-  document.querySelector('.total-clicks-ever').innerHTML = totalClicksEver;
-  document.getElementById('stats-total-powerups-golden').innerHTML = totalPowerupsGolden;
 }
